4422d7d1c29d50c586c6bfb65d16d8b323d0c6f5
MarcelK <59581396+LetsPoke@users.noreply.github.com>
2024-03-02 17:03:28 +0100

    improve lang switch and socials

--- Portfolio_Page/index.js
+++ Portfolio_Page/index.js
@@ -1,6 +1,8 @@
 function switchLanguage(lang) {
     document.querySelectorAll('.language').forEach(function (el) {
-        el.style.display = 'none' // Hide all language sections
+        el.style.display = 'none'
+        el.classList.remove('selected')
     })
-    document.getElementById('content-' + lang).style.display = 'block' // Show the selected language
+    document.getElementById('content-' + lang).style.display = 'block'
+    document.getElementById('content-' + lang).classList.add('selected')
 }
